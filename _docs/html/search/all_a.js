434a3fbe8c113047f951f1cf4170457fff71f85f
Domingo1987 <domingoperez1987@gmail.com>
2023-03-23 20:13:54 -0300

    new doxy solo se puede docs

--- _docs/html/search/all_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['programación_202_0',['Programación 2',['../md_README.html',1,'(Namespace global)'],['../md_tabla_contenidos.html',1,'(Namespace global)'],['../md_TablaContenidos.html',1,'(Namespace global)']]]
-];
--- _docs/html/search/all_d.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['tabla_2dcontenidos_2emd_0',['tabla-contenidos.md',['../tabla-contenidos_8md.html',1,'']]],
-  ['tablacontenidos_2emd_1',['TablaContenidos.md',['../TablaContenidos_8md.html',1,'']]]
-];
