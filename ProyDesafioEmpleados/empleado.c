593653a78ffc80937affb61567593d48d709aad0
Domingo1987 <domingoperez1987@gmail.com>
2023-03-24 13:02:18 -0300

    header

--- ProyDesafioEmpleados/empleado.c
+++ ProyDesafioEmpleados/empleado.c
@@ -1,10 +1,3 @@
-/**
- * @file empleado.c
- * @brief Definicion de funciones de empleado.h
- * @author Domingo Pérez
- * @date 22 de marzo de 2023
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
